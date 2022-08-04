5557ff86897663efb8b0f62b256ba758902d776d
Jim Drury <jim.drury@virginmediao2.co.uk>
2022-08-04 12:35:24 +0100

    feat(scaffolding): adds scaffolding for 404 page and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Route, RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const home: Route = {
+  path: '',
+  pathMatch: 'full',
+  loadChildren: () => import('./pages/home/home.module').then(mod => mod.HomeModule),
+}
+
+const notFound: Route = {
+  path: '**',
+  loadChildren: () => import('./pages/not-found/not-found.module').then(mod => mod.NotFoundModule),
+}
+
+const routes: Routes = [
+  home,
+  notFound,
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {initialNavigation: 'enabledBlocking'})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
